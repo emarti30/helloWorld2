0628e1917c9025005fca7a0fe0cd7730f2ce7ee3
emarti30 <emarti30@terpmail.umd.edu>
2026-09-10 19:07:07 -0400

    Updated main page text

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World from Emely Martinez!'
+    return 'Hello World from Emely Martinez! I am adding my first code change.'
 
 @app.route('/hello')
 def hello():
